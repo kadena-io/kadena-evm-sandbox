2132315ef2aaf90c2d1050da7f7aa339befc5591
Isa Ozler <isaozler@gmail.com>
2025-02-06 14:13:19 +0100

    panel component

--- apps/kethamp/src/app/page.tsx
+++ apps/kethamp/src/app/page.tsx
@@ -1,8 +1,10 @@
+import Panel from "@app/components/panel/panel";
+
 export default function Main() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-      <main className="">
-        
+      <main className="w-full m-auto max-w-[640px]">
+        <Panel />
       </main>
     </div>
   );
---
+++ apps/kethamp/src/components/panel/panel.tsx
@@ -0,0 +1,9 @@
+import Header from "@app/components/panel/header/header";
+
+export default function Panel() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}

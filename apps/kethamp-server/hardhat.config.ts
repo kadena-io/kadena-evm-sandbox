bda0d5973ab1486c0bb9359b848998cbbbec9cd3
Andy Tang <andy@enof.solutions>
2025-02-06 20:00:14 +0100

    fix accounts import

--- apps/kethamp-server/hardhat.config.ts
+++ apps/kethamp-server/hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-watcher";
 import "hardhat-switch-network";
-import { accounts } from "./accounts.json";
+import data from "./accounts.json" with { type: "json" };
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -26,12 +26,12 @@ const config: HardhatUserConfig = {
     kadena_devnet1: {
       url: "http://localhost:8545",
       chainId: 1789,
-      accounts: accounts.map(({ privateKey }) => privateKey),
+      accounts: data.accounts.map(({ privateKey }) => privateKey),
     },
     kadena_devnet2: {
       url: "http://localhost:8555",
       chainId: 1790,
-      accounts: accounts.map(({ privateKey }) => privateKey),
+      accounts: data.accounts.map(({ privateKey }) => privateKey),
     },
   },
 };

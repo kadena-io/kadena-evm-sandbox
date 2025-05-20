6ea92f4986c2d8b8730f252de24b6d94c5907ffa
Heather S <42716568+hswopeams@users.noreply.github.com>
2025-05-20 18:17:01 -0400

    Configure chainweb sandbox with 1 chain in hardhat config

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -2,13 +2,46 @@ require('@nomicfoundation/hardhat-toolbox');
 require('hardhat-switch-network');
 require('@nomicfoundation/hardhat-verify');
 require('@kadena/hardhat-chainweb');
+const { readFileSync } = require("fs");
+
+const devnetAccounts = JSON.parse(
+  readFileSync("./devnet-accounts.json", "utf-8")
+);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.28',
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+      evmVersion: "prague",
+    },
+  },
   chainweb: {
     hardhat: {
       chains: 2,
     },
+    devnet: {
+      type: 'external',
+      chains: 2,
+      accounts: devnetAccounts.accounts.map((account) => account.privateKey),
+      chainIdOffset: 1789,
+      externalHostUrl:
+        "https://evm-devnet.kadena.network/chainweb/0.0/evm-development",
+    },
+     sandbox: {
+      type: 'external',
+      chains: 1,
+      accounts: devnetAccounts.accounts.map((account) => account.privateKey),
+      chainIdOffset: 1789,
+      externalHostUrl:
+        "http://localhost:8545",
+    },
   },
+  mocha: {
+    timeout: 300000
+  }
 };
